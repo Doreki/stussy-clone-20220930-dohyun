aa952a1e6893fb5eb0b14539983a03141bb89d96
Doraeki <idh0325@naver.com>
2022-09-30 17:54:59 +0900

    프론트 영역 로그인 창 구현

---
+++ src/test/java/com/stussy/stussyclone20220930dohyun/StussyClone20220930DohyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220930dohyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyClone20220930DohyunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
