fc336e2e961f295fde4243c464042ee7fd3951d3
Doraeki <idh0325@naver.com>
2022-10-05 17:52:18 +0900

    validation

--- src/main/java/com/stussy/stussyclone20220930dohyun/api/AccountApi.java
+++ src/main/java/com/stussy/stussyclone20220930dohyun/api/AccountApi.java
@@ -1,17 +1,39 @@
 package com.stussy.stussyclone20220930dohyun.api;
 
 import com.stussy.stussyclone20220930dohyun.dto.RegisterReqDto;
+import com.stussy.stussyclone20220930dohyun.dto.validation.ValidationSequence;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/api/account")
 @RestController
 public class AccountApi {
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterReqDto registerReqDto) {
-        System.out.println("회원가입 요청 데이터:" + registerReqDto);
-        return null;
+    public ResponseEntity<?> register(@Validated(ValidationSequence.class) @RequestBody RegisterReqDto registerReqDto, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                System.out.println("fieldError = " + fieldError.getField());
+                System.out.println("fieldError.getDefaultMessage() = " + fieldError.getDefaultMessage());
+                errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+
+            return ResponseEntity.badRequest().body(errorMap);
+        }
+
+        return ResponseEntity.created(null).body(null);
     }
 }
